a7bcbcd39fd755c5f1a77c714f62c54158805038
abhishek2x <abhisheksrivastavabbn@gmail.com>
2021-01-16 12:09:13 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-4916450f49a239b6ea2e.js"
+    "url": "webpack-runtime-5d267b95c55bd48a593e.js"
   },
   {
     "url": "framework-b93d1b69564797e828f3.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "3bb2f5d05f067321708ba55fb586e112"
+    "revision": "80d19361dd06a6dcd1183570cbfbab58"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-69cdedfea3cef0fb395c.js"
